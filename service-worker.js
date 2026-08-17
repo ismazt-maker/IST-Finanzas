b29f8848de9c77d3ecd6d4e679e4f8d096ed43d3
ismazt-maker <isma.zt@gmail.com>
2026-08-17 14:53:22 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="ist-v2131-clean";
+const CACHE="ist-v2132-clean";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-120.png","./icon-152.png","./icon-167.png","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
